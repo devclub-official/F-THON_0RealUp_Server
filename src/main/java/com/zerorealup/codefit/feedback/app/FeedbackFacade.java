9812feaf8bb69c729e10fe51eff4ceeee17c177a
codrin2 <hssang127@gmail.com>
2025-04-19 05:03:35 +0900

    feat: createCodeFeedback 서비스 로직 구현 #3

--- src/main/java/com/zerorealup/codefit/feedback/app/FeedbackFacade.java
+++ src/main/java/com/zerorealup/codefit/feedback/app/FeedbackFacade.java
@@ -1,6 +1,8 @@
 package com.zerorealup.codefit.feedback.app;
 
+import com.zerorealup.codefit.feedback.domain.Feedback;
 import com.zerorealup.codefit.feedback.domain.service.AiUsageService;
+import com.zerorealup.codefit.feedback.domain.service.FeedbackService;
 import com.zerorealup.codefit.feedback.infra.NaturalLanguageProcessor;
 import com.zerorealup.codefit.member.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +12,34 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FeedbackFacade {
     private final MemberService memberService;
+    private final FeedbackService feedbackService;
     private final AiUsageService aiUsageService;
     private final NaturalLanguageProcessor processor;
 
-    public String createCodeFeedback(Long memberId, String code) {
+    // todo: event로 받아 처리
+    public Feedback createCodeFeedback(
+            Long memberId, String problemTitle, String difficulty, String code
+    ) {
         memberService.findExistingMember(memberId);
         aiUsageService.increaseUsage(memberId);
+
+        String feedbackResult = processor.createCodeFeedback(code);
+
+        Feedback feedback = Feedback.createFeedback(
+                problemTitle,
+                difficulty,
+                feedbackResult
+        );
+
+        return feedbackService.save(feedback);
+    }
+
+    public String testCodeFeedback(
+            Long memberId, String code
+    ) {
+        memberService.findExistingMember(memberId);
+        aiUsageService.increaseUsage(memberId);
+
         return processor.createCodeFeedback(code);
     }
 }
