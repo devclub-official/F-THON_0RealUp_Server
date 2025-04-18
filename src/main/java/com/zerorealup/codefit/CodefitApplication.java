35542a4f59288985147b5aca89f1ac901dd05e22
공수정 <brightelf9@naver.com>
2025-04-19 00:57:55 +0900

    init

---
+++ src/main/java/com/zerorealup/codefit/CodefitApplication.java
@@ -0,0 +1,13 @@
+package com.zerorealup.codefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefitApplication.class, args);
+	}
+
+}
